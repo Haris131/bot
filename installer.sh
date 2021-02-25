d345b37b0fc6041c21d98df361a929d781e736c4
Haris131 <41672454+Haris131@users.noreply.github.com>
2021-02-25 19:47:10 +0700

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+mkdir /root/watchhours
+wget -O /root/watchhours/watchhours.py https://raw.githubusercontent.com/Haris131/bot/main/watchhours.py
+wget -O /root/watchhours/cfg.json https://raw.githubusercontent.com/Haris131/bot/main/cfg.json
+rm /root/installer
+echo -e "install bot selesai"
